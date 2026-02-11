2aa1a7eff1dc2b52d186ecbe5d53365630bfdd5c
Corentin Lefrere <clefrere@k1r1p13.42mulhouse.fr>
2026-02-11 10:16:27 +0100

    modification of maze class creation to add exit and entry point

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -13,7 +13,7 @@ def is_valid_config(config: Dict) -> bool:
     return True
 
 
-def main(config_file:str="config.txt"):
+def main(config_file: str = "config.txt"):
     config = read_config_file(config_file)
     if not is_valid_config(config):
         print("Invalid configuration, please check")
@@ -22,17 +22,18 @@ def main(config_file:str="config.txt"):
         print("Configuration is missing! update the config.txt file.")
         return
     print(config)
-    maze = Maze(int(config.get('WIDTH')), int(config.get('HEIGHT')))
     perfect = config.get('PERFECT')
     entry = config.get('ENTRY')
     entry = tuple(map(int, entry.split(',')))
     exit = config.get('EXIT')
     exit = tuple(map(int, exit.split(',')))
+    maze = Maze(int(config.get('WIDTH')), int(config.get('HEIGHT')),
+                entry, exit)
 
     if perfect is True:
         maze.create_perfect_maze()
     else:
-        maze.create_imperfect_maze(entry, exit)
+        maze.create_imperfect_maze()
     maze.print_maze()
     maze.generate_maze_output()
     print_maze()
